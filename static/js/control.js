942f9b5da73b6cb0344bd3256eee8a18825441f6
Bit_zhouYongYang <504524694@qq.com>
2024-01-26 15:40:01 +0800

    add renderer code

--- static/js/control.js
+++ static/js/control.js
@@ -1,7 +1,7 @@
 
 // const rendererURL = 'http://127.0.0.1:5500/renderer_vosh/renderer.html'
-const assetsURL = 'https://huggingface.co/zachzhang07/Vosh/tree/main'
-const rendererURL = "./renderer_vosh.html";
+const assetsURL = 'https://huggingface.co/zachzhang07/Vosh/tree/main';
+const rendererURL = "https://zhou-yongyang.github.io/renderer_vosh/renderer.html";
 
 // const rendererURL = 'https://huggingface.co/zachzhang07/Vosh/tree/main/'
 function handleGardenButtonClick(){
